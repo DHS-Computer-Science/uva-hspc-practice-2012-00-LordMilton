c69dccba0d27a514855ea16e98322f133bbf839d
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-12-09 20:32:21 -0500

    Added sample files

---
+++ Main.java
@@ -0,0 +1,12 @@
+//First_Name Last_Name
+//YYYY-MM-DD
+//Practice-2012-00
+
+public class Main {
+  public static void main(String[] args) {
+
+    //TODO - your code goes here
+
+    System.exit(0);
+  }
+}
